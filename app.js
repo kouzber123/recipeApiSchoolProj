0707af4225b9a70101f5f81f4a5d38d7a6a5c469
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2022-03-19 15:35:53 +0200

    prettifying files

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //-----------------routes
-// for user selected food id
-var lookFor = {
-  label: String,
-};
+
+//weather app keys , setups / 1st request is just placeholder for the page
+
 const key = "c0079132d6f5ae19bb06dadb73eca002";
 const units = "metric";
+const weather_url = `https://api.openweathermap.org/data/2.5/find?q=helsinki&appid=${key}&units=${units}`;
+
+//parse data and get images into seperate entities for easier printing
 app.get("/", (req, res) => {
-  const url = `https://api.openweathermap.org/data/2.5/find?q=helsinki&appid=${key}&units=${units}`;
-  https.get(url, (response) => {
+  https.get(weather_url, (response) => {
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       const icon = weatherData.list[0].weather[0].icon;
@@ -26,11 +27,12 @@ app.get("/", (req, res) => {
     });
   });
 });
-//get requested city/country
+
+//get requested city/country > this the dynamic part
 app.post("/", (req, res) => {
   const country = req.body.country;
-  const url = `https://api.openweathermap.org/data/2.5/find?q=${country}&appid=${key}&units=${units}`;
-  https.get(url, (response) => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/find?q=${country}&appid=${key}&units=${units}`;
+  https.get(weatherUrl, (response) => {
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
 
@@ -40,32 +42,37 @@ app.post("/", (req, res) => {
     });
   });
 });
+
+//........................food site starts
+
+//user selected food id
+var lookForFood = { recipe: String };
+
+//api keys
+const recipeKey = "ea682f9966e033c3ee883f0db5950740";
+const recipeID = "d5d60950";
 
 app.get("/foods", (req, res) => {
   res.render("foods");
 });
 
+//https req to the edamam with userinput from the url.
 app.post("/foods", (req, res) => {
-  //fetch data from edamam
   const recipe = req.body.recipe;
-  const recipeKey = "ea682f9966e033c3ee883f0db5950740";
-  const recipeID = "d5d60950";
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${recipe}&app_id=${recipeID}&app_key=${recipeKey}`;
+
   https
     .get(url, (response) => {
       let data = "";
-      // A chunk of data has been received.
-      response.on("data", (chunk) => {
-        data += chunk;
+
+      response.on("data", (recipeData) => {
+        data += recipeData;
       });
-      // The whole response has been received. Print out the result.
-      //render new file
+      // The whole response has been received. parse data and add into object for later use, data.hits > prettify results
       response.on("end", () => {
         data = JSON.parse(data);
         const foodData = data.hits;
-        lookFor = {
-          label: foodData,
-        };
+        lookForFood = { recipe: foodData };
         res.render("foodpost", { foodData: foodData });
       });
     })
@@ -73,8 +80,11 @@ app.post("/foods", (req, res) => {
       console.log("Error: " + err.message);
     });
 });
-app.get("/foodName/:foodlabel/id/:foodID", (req, res) => {
-  const targetArray = lookFor.label;
+
+//to create new page from the recipe user wants,
+//use lookForFood where we saved api data and get data that matches the index, index ? params.id : render the data
+app.get("/id/:foodID", (req, res) => {
+  const targetArray = lookForFood.recipe;
 
   const index = req.params.foodID;
   if (targetArray[index]) {
